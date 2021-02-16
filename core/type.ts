1fdc7bdd13f9304ac506a4e4f90a87b94cd580c7
makotofukuda <makoto.fukuda@nulab-inc.com>
2021-02-17 05:49:49 +0900

    remove type

--- core/type.ts
+++
@@ -1,4 +0,0 @@
-export type JSXChild = JSX.Element | string | number | null;
-export interface DeepArrayI<T> extends Array<T | DeepArrayI<T>> {}
-export type DeepArray<T> = T | DeepArrayI<T>;
-export type JSXChildren = DeepArray<JSXChild>;
